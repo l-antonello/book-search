eb68eff55b5e45a326ea40e392d172e2ae6c6c23
otterslacks <antonelloluke@gmail.com>
2023-08-20 17:09:02 -0400

    typedefs and resolvers to index.js

---
+++ Develop/server/schemas/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+const typeDefs = require('./graphql/typeDefs'); // Import your GraphQL schema type definitions
+const resolvers = require('./graphql/resolvers'); // Import your GraphQL resolvers
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Create an Apollo Server instance
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    // You can extract and pass authentication-related information from the request here
+  },
+});
+
+// Apply Apollo Server middleware to Express
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
+
+module.exports = { typeDefs, resolvers };
